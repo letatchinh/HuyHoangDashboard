8b2071885436b8d04f56a2de0ca568b09316b4da
CuongVo <cuongvoquocc@gmail.com>
2024-01-09 22:58:37 +0700

    feat: upload image components

--- src/constants/defaultValue.ts
+++ src/constants/defaultValue.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = 'https://api.congtyso.com'
+export const BASE_URL = 'https://api.congtyso.com';
+
+export const DEFAULT_UPLOAD_ACTION = `${BASE_URL}/api/v1/file`;
+export const MAX_UPLOAD_FILE_SIZE_IN_MB = 2;
