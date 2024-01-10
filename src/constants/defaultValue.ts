d774ad9c3a6795060817e284818d9d0534ffae4b
Mai Ngoc Anh <mtna.2302@gmail.com>
2024-01-10 17:09:59 +0700

    fix: #1226 - add status default value

--- src/constants/defaultValue.ts
+++ src/constants/defaultValue.ts
@@ -1,1 +1,11 @@
-export const BASE_URL = 'https://api.congtyso.com'
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+export const STATUS = {
+  ACTIVE: "ACTIVE",
+  INACTIVE: "INACTIVE",
+};
+
+export const STATUS_NAMES = {
+  ACTIVE: "Hoạt động",
+  INACTIVE: "Ngưng hoạt động",
+};
