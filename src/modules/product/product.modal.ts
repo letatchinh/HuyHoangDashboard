8fda9c93d038c91dbdb4f29cff2b3989f5a45e4a
Lê Tất Chính <letatchinh123@gmail.com>
2024-01-11 20:47:55 +0700

    feat: #1281[Nhà cung cấp] Cấu hình chiết khấu - config discount form

--- src/modules/product/product.modal.ts
+++ src/modules/product/product.modal.ts
@@ -6,16 +6,42 @@ export type TypePropsFormProduct = {
 export type TypePropsListProduct = {
     supplierId?:string,
 }
-type variantType = {
+export type variantType = {
     productUnit : string,
     productId : string,
     exchangeValue : number,
     price : number,
+    cost : number,
     variantIsDefault : boolean,
+}
+export type conditionType = {
+  gte : number,
+  lte?:number,
+  isRanger : boolean,
+}
+export type applyTimeSheetType = {
+  gte : number,
+  lte?:number,
+  isRepeat : boolean,
+}
+export type cumulativeDiscountType = {
+  target : string,
+  targetId : string,
+  name : string,
+  typeDiscount : string,
+  value : number,
+  valueType : string,
+  typeReward : string,
+  condition : conditionType | null,
+  applyUnit : string | null,
+  applyTimeSheet : applyTimeSheetType | null
+
+
 }
 export type FieldTypeFormProduct = {
     name: string;
     type: string;
+    saleLevel: string;
     supplierId: string;
     productDetail : {
         package : string,
@@ -25,10 +51,17 @@ export type FieldTypeFormProduct = {
     manufacturerId : string,
     productGroupId : string,
     medicalCode : string,
-    variants : variantType[]
+    variants : variantType[],
+    cumulativeDiscount : cumulativeDiscountType[]
   };
 
   export type TypePropVariants = {
     form : any,
     isLoading : boolean
+  }
+
+  export type TypePropsDiscountList = {
+    product?: any,
+    loading? : any,
+    form : any
   }
