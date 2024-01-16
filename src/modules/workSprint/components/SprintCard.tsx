33d9a8321ff13a8abad71d783fc99765bf34d9e0
Matt Le <manhnghean183286@gmail.com>
2024-01-16 09:37:11 +0700

    feat:feat sprint

--- src/modules/workSprint/components/SprintCard.tsx
+++ src/modules/workSprint/components/SprintCard.tsx
@@ -1,82 +1,86 @@
-
 import React, { useEffect, useState } from 'react';
+// import { useHistory } from 'react-router-dom';
+import { Button, Form, Input, Tooltip, Typography } from 'antd';
 import { CloseCircleFilled, FormOutlined } from '@ant-design/icons';
-import { Button, Form, Input, Space, Tooltip, Typography } from 'antd';
+// import useNavigate from '~/hooks/useNavigate';
 import { useNavigate } from 'react-router-dom';
-import POLICIES from '~/modules/policy/policy.auth';
+// import { WithPermission } from '~/components/Common';
+// import POLICIES from '~/constants/policy';
+
 interface SprintCardProps {
   name: string;
   note: string;
   type: string;
-  key: string;
   onCreate: (data: { name: string; note: string }) => void;
   onSave: (data: { name: string; note: string; id: string }) => void;
   onDelete: (data: { id: string }) => void;
   id: string;
-  boardId?: string;
+  // boardId: string;
   style: any;
   userIsAdminforBoard: boolean;
 }
 
-// const SprintCard: React.FC<SprintCardProps> = ({
-  export default function SprintCard({
-    name,
-    note,
-    type,
-    onCreate,
-    onSave,
-    onDelete,
-    id,
-    boardId,
-    style,
-    userIsAdminforBoard,
-  }: SprintCardProps): React.JSX.Element {
-    const [note_, setNote] = useState(note);
-    const [name_, setName] = useState(name);
-    const [vis, setVis] = useState(false);
-    const navigate = useNavigate();
-    function handleSaveNote() {
-      setVis(false);
-      if (type === 'CREATE') {
-        onCreate({ name: name_, note: note_ });
-      } else if (name_ !== name || note_ !== note) {
-        onSave({ name: name_, note: note_, id });
-      } else {
-        console.log('out');
-      }
+const SprintCard: React.FC<SprintCardProps> = ({
+  name,
+  note,
+  type,
+  onCreate,
+  onSave,
+  onDelete,
+  id,
+  // boardId,
+  style,
+  userIsAdminforBoard,
+}) => {
+  const [note_, setNote] = useState<string>(note);
+  const [name_, setName] = useState<string>(name);
+  const [vis, setVis] = useState<boolean>(false);
+
+  function handleSaveNote(data: { name: string; note: string }) {
+    setVis(false);
+    if (type === 'CREATE') {
+      onCreate({ name: data.name, note: data.note });
+    } else if (name_ !== name || note_ !== note) {
+      onSave({ name: name_, note: note_, id });
+    } else {
+      console.log('out');
     }
-    useEffect(() => {
-      setName(name);
-      setNote(note);
-    }, [name, note]);
-  
-    const validNote = () => {
-      setVis(false);
-    };
-  
-    if (type === 'CREATE') return <Create handleSaveNote={handleSaveNote} />;
-    return (
-      <Detail
-        {...{
-          name_,
-          note_,
-          setName,
-          setNote,
-          setVis,
-          vis,
-          handleSaveNote,
-          note,
-          id,
-          boardId,
-          onDelete,
-          style,
-          userIsAdminforBoard,
-        }}
-      />
-    );
+  }
+
+  useEffect(() => {
+    setName(name);
+    setNote(note);
+  }, [name, note]);
+
+  const validNote = (value: string) => {
+    setVis(false);
   };
+
+  if (type === 'CREATE') return <Create handleSaveNote={handleSaveNote} />;
+
+  return (
+    <Detail
+      name_={name_}
+      note_={note_}
+      setName={setName}
+      setNote={setNote}
+      setVis={setVis}
+      vis={vis}
+      handleSaveNote={handleSaveNote}
+      note={note}
+      id={id}
+      // boardId={boardId}
+      onDelete={onDelete}
+      style={style}
+      userIsAdminforBoard={userIsAdminforBoard}
+    />
+  );
+};
+
+export default SprintCard;
+
 interface CreateProps {
-  handleSaveNote: () => void;
+  handleSaveNote: (data: { name: string; note: string }) => void;
 }
 
 const Create: React.FC<CreateProps> = ({ handleSaveNote }) => {
@@ -86,7 +90,7 @@ const Create: React.FC<CreateProps> = ({ handleSaveNote }) => {
     <div className="sprint-card-container">
       <Form
         form={form}
-        onFinish={(value) => handleSaveNote()}
+        onFinish={(value) => handleSaveNote(value)}
         layout="horizontal"
         labelCol={{ span: 24 }}
       >
@@ -112,10 +116,10 @@ interface DetailProps {
   name_: string;
   note_: string;
   setName: React.Dispatch<React.SetStateAction<string>>;
-  setNote: React.Dispatch<React.SetStateAction<string>>;
-  validNote?: () => void;
+  // validNote: (value: string) => void;
   setVis: React.Dispatch<React.SetStateAction<boolean>>;
-  handleSaveNote: () => void;
+  setNote: React.Dispatch<React.SetStateAction<string>>;
+  handleSaveNote: (data: { name: string; note: string }) => void;
   vis: boolean;
   note: string;
   id: string;
@@ -129,26 +133,22 @@ const Detail: React.FC<DetailProps> = ({
   name_,
   note_,
   setName,
-  validNote,
   setVis,
   setNote,
   handleSaveNote,
   vis,
   note,
   id,
-  boardId,
+  // boardId,
   onDelete,
   style,
   userIsAdminforBoard,
 }) => {
-  const navigate = useNavigate();
-  const [focusName, setFocusName] = useState(false);
-
+  // const history = useHistory();
+  const [focusName, setFocusName] = useState<boolean>(false);
+const navigate = useNavigate()
   return (
-    <div
-      className="sprint-card-container detail"
-      style={{ ...style }}
-    >
+    <div className="sprint-card-container detail" style={{ ...style }}>
       {/* <WithPermission permission={POLICIES.DELETE_TODOLIST}> */}
         <div className="sprint-card-close">
           <CloseCircleFilled
@@ -162,35 +162,36 @@ const Detail: React.FC<DetailProps> = ({
       {/* </WithPermission> */}
       <div
         className="sprint-card-title"
-        accessKey="true"
         onClick={(e) => {
           e.stopPropagation();
           e.preventDefault();
           if (e.currentTarget.accessKey === 'true') {
-            navigate(`/work-board/detail/${id}`);
+            navigate(`/work-flow/detail/${id}`);
           }
         }}
         style={{ cursor: 'pointer' }}
       >
-       { !focusName ? <>
-            <Tooltip
-            title={name_}
-            mouseEnterDelay={1.2}
-            overlayStyle={{ fontSize: 12 }}
-          >
-            <Typography.Title accessKey='true' className='sprint-card-title_link'>{name_}</Typography.Title>
-          </Tooltip>
-          {
-              // userIsAdminforBoard &&(
-                <FormOutlined onClick={()=>{
+        {!focusName ? (
+          <>
+            <Tooltip title={name_} mouseEnterDelay={1.2} overlayStyle={{ fontSize: 12 }}>
+              <Typography.Title accessKey="true" className="sprint-card-title_link">
+                {name_}
+              </Typography.Title>
+            </Tooltip>
+            {/* {userIsAdminforBoard && ( */}
+              <FormOutlined
+                onClick={() => {
                   setVis(true);
-                  setFocusName(true)
-                }} color='blue' className='sprint-card-title_editor' />
-              // )
-          }
-
-          </> : <Input value={name_} onChange={(value)=>setName(value.target.value)} style={{width:'100%'}}></Input>}
-         
+                  setFocusName(true);
+                }}
+                color="blue"
+                className="sprint-card-title_editor"
+              />
+            {/* )} */}
+          </>
+        ) : (
+          <Input value={name_} onChange={(value) => setName(value.target.value)} style={{ width: '100%' }} />
+        )}
       </div>
 
       <Input.TextArea
@@ -210,7 +211,7 @@ const Detail: React.FC<DetailProps> = ({
             htmlType="button"
             onClick={() => {
               setFocusName(false);
-              handleSaveNote();
+              handleSaveNote({ name: name_, note: note_ });
             }}
             type="primary"
             size="small"
