a89ecb7e075509ce214b606d0a317e1e9003d7d0
CuongVo <cuongvoquocc@gmail.com>
2024-01-17 10:57:21 +0700

    feat: add ckeditor

---
+++ src/utils/Editors.tsx
@@ -0,0 +1,24 @@
+// import React from 'react'
+// import { Editor } from '@ckeditor/ckeditor5-core';
+// import { CKEditor } from '@ckeditor/ckeditor5-react';
+// import CustomEditor from 'ckeditor5-custom-build/build/ckeditor';
+
+// interface PropsEditor {
+//   value?: any,
+//   onChange?: any
+// };
+
+// function Editors({value,onChange}: PropsEditor){
+//     return (
+//         <CKEditor
+//             id={'ckEditor5-editor'}
+//             editor={CustomEditor}
+//             data={value}
+//             onChange={(_ : any, editor : any) => {
+//             onChange(editor.getData());
+//           }}
+//         />        
+//     )
+// };
+
+// export default Editors;
