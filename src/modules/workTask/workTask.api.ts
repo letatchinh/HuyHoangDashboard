4453a9fda112e7bba93f8e1d31b56b545f9dd216
CuongVo <cuongvoquocc@gmail.com>
2024-01-17 09:49:43 +0700

    feat: #1290 fix api

--- src/modules/workTask/workTask.api.ts
+++ src/modules/workTask/workTask.api.ts
@@ -6,10 +6,10 @@ const apis = {
     //GET
     getAll: (query?: any) => requester.get(`/api/v1/task-item`, query),
     getById: (id?: any) => requester.get(`/api/v1/task-item/${id}`),
-    getRelationTask: (taskId: any) => requester.get(`/relation-task-item/${taskId}`),
-    getHistoryTaskById: (id: any) => requester.get(`history-todo-task/${id}`),
-    searchTaskByBoardId: ({ boardId, keyword, taskId }: any) => requester.post(`/search-board-task-item/${boardId}`, { keyword, taskId }),
-    getAllManagersByIdBoard: (id: any) => requester.get(`/auth-todo-list-role-admin/${id}`),
+    getRelationTask: (taskId: any) => requester.get(`/api/v1/relation-task-item/${taskId}`),
+    getHistoryTaskById: (id: any) => requester.get(`/api/v1/history-todo-task/${id}`),
+    searchTaskByBoardId: ({ boardId, keyword, taskId }: any) => requester.post(`/api/v1/search-board-task-item/${boardId}`, { keyword, taskId }),
+    getAllManagersByIdBoard: (id: any) => requester.get(`/api/v1/auth-todo-list-role-admin/${id}`),
 
     //CREATE
     create: (data?: any) => requester.post(`/api/v1/task-item`, data),
@@ -17,16 +17,16 @@ const apis = {
     copyTask: ({ id, ...data }: any) => requester.post(`/task-item-copy/${id}`, data),
     
     //UPDATE
-    pushComment: ({ taskId, content }: any) => requester.post(`/process-add-comment-task-item/${taskId}`, { content }),
-    pushEmotion: ({ taskId, user, commentId }: any) => requester.put(`/process-emotion-comment-task-item/${taskId}?commentId=${commentId}`, user),
-    updateRelationTask: (body: any) => requester.put(`/update-relation-task-item/${body.taskItemId}`, pick(body, ['typeRequest', 'taskId'])),
-    updateTask: ({ id, ...data }: any) => requester.put(`/task-item/${id}`, data),
-    updateCommentById: ({ taskId, content, commentId }: any) => requester.put(`/process-update-comment-task-item/${taskId}?commentId=${commentId}`, { content }),
-    updateProgressTask: ({ id, ...data }: any) => requester.put(`/task-item-progress/${id}`, data),
+    pushComment: ({ taskId, content }: any) => requester.post(`/api/v1/process-add-comment-task-item/${taskId}`, { content }),
+    pushEmotion: ({ taskId, user, commentId }: any) => requester.put(`/api/v1/process-emotion-comment-task-item/${taskId}?commentId=${commentId}`, user),
+    updateRelationTask: (body: any) => requester.put(`/api/v1/update-relation-task-item/${body.taskItemId}`, pick(body, ['typeRequest', 'taskId'])),
+    updateTask: ({ id, ...data }: any) => requester.put(`/api/v1/task-item/${id}`, data),
+    updateCommentById: ({ taskId, content, commentId }: any) => requester.put(`/api/v1/process-update-comment-task-item/${taskId}?commentId=${commentId}`, { content }),
+    updateProgressTask: ({ id, ...data }: any) => requester.put(`/api/v1/task-item-progress/${id}`, data),
     
     //DELETE
     delete: (id?: any) => requester.delete(`/api/v1/task-item/${id}`),
-    deleteComment: ({ taskId, commentId }: any) => requester.delete(`/process-delete-comment-task-item/${taskId}?commentId=${commentId}`),
+    deleteComment: ({ taskId, commentId }: any) => requester.delete(`/api/v1/process-delete-comment-task-item/${taskId}?commentId=${commentId}`),
 
 };
 export default apis;
