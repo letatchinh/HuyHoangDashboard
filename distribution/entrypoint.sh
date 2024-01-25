a09cd56049bfe55fdf7f211b1ea5e13047c378a1
Daniel Pham <nguyen.pc@live.com>
2024-01-26 00:37:37 +0700

    feat: update entrypoint

--- distribution/entrypoint.sh
+++ distribution/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-/root/config.sh /var/www/static/js/main.*.chunk.js
+/root/config.sh /var/www/static/js/main.*.js
 /root/config.sh /var/www/index.html
-/root/config.sh /var/www/firebase-messaging-sw.js
 
 exec "$@"
 
