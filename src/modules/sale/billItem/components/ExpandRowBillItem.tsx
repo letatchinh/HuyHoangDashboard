31f7b093c33cb0467e7d261d4fa53c6f6a42d474
Lê Tất Chính <letatchinh123@gmail.com>
2024-02-01 08:22:27 +0700

    feat: show discount in bill Detail

--- src/modules/sale/billItem/components/ExpandRowBillItem.tsx
+++ src/modules/sale/billItem/components/ExpandRowBillItem.tsx
@@ -1,13 +1,19 @@
-import { Divider } from 'antd';
-import { omit } from 'lodash';
+import { Divider, Statistic, Table, Typography } from 'antd';
+import { get, omit } from 'lodash';
 import React from 'react';
+import { TARGET_VI, TYPE_DISCOUNT_VI, TYPE_VALUE } from '~/modules/cumulativeDiscount/constants';
+import { formatter } from '~/utils/helpers';
 import StepStatus from '../../bill/components/StepStatus';
 import { STATUS_BILLITEM, STATUS_BILLITEM_VI } from '../constants';
 type propsType = {
     historyStatus : any,
     status : any,
+    cumulativeDiscount : any,
 }
-export default function ExpandRowBillItem({historyStatus,status}:propsType) : React.JSX.Element {
+const CLONE_TARGET_VI :any = TARGET_VI; 
+const CLONE_TYPE_DISCOUNT_VI :any = TYPE_DISCOUNT_VI; 
+export default function ExpandRowBillItem({historyStatus,status,cumulativeDiscount}:propsType) : React.JSX.Element {
+  
     return (
         <div>
             <StepStatus
@@ -29,6 +35,51 @@ export default function ExpandRowBillItem({historyStatus,status}:propsType) : Re
               historyStatus={historyStatus}
             />
             <Divider />
+            <h5>Chiết khấu</h5>
+            <Table 
+            dataSource={cumulativeDiscount}
+            columns={[
+              {
+                title : 'Tên chiết khấu',
+                dataIndex : 'name',
+                key : 'name',
+              },
+              {
+                title : 'Loại chiết khấu',
+                dataIndex : 'typeDiscount',
+                key : 'typeDiscount',
+                render(typeDiscount) {
+                  return  <span>{CLONE_TYPE_DISCOUNT_VI[typeDiscount]}</span>
+                }
+              },
+              {
+                title : 'Chiết khấu của',
+                dataIndex : 'target',
+                key : 'target',
+                render(target) {
+                  return  <span>{CLONE_TARGET_VI[target]}</span>
+                }
+              },
+              {
+                title : 'Giá trị chiết khấu',
+                dataIndex : 'value',
+                key : 'value',
+                align : 'center',
+                render(value,record) {
+                  return  <span>{formatter(value)}{get(record,'valueType') === TYPE_VALUE.PERCENT ? '%' : ''}</span>
+                }
+              },
+              {
+                title : 'Tổng giá trị chiết khấu',
+                dataIndex : 'discountAmount',
+                key : 'discountAmount',
+                align : 'center',
+                render (discountAmount) {
+                  return <Typography.Text strong>{formatter(discountAmount)}</Typography.Text>
+                }
+              },
+            ]}
+            />
         </div>
     )
 }
