7d7e8bb89b0b927567eb8191a882e2e0e1f83299
Lê Tất Chính <letatchinh123@gmail.com>
2024-02-19 09:26:47 +0700

    fix: #1345 - split Tab

---
+++ src/modules/supplier/components/HistoryReport.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+type propsType = {
+
+}
+export default function HistoryReport(props:propsType) : React.JSX.Element {
+    return (
+        <div>HistoryReport</div>
+    )
+}
