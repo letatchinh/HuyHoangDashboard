eb5efb4e272dd268d32e9535eed38010b0809754
Lê Tất Chính <letatchinh123@gmail.com>
2024-02-20 21:02:14 +0700

    feat: #1356 - Create Quick Action create quotaion, create order Supplier and Create medicine

--- src/modules/homepage/screens/Homepage.tsx
+++ src/modules/homepage/screens/Homepage.tsx
@@ -1,7 +1,8 @@
 import { DropboxOutlined, InboxOutlined, MedicineBoxOutlined } from "@ant-design/icons";
-import { Col, Row } from "antd";
-import React, { useCallback } from "react";
+import { Col, Modal, Row } from "antd";
+import React, { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import ShowStep from "~/modules/productsAll/components/ShowStep";
 import { PATH_APP } from "~/routes/allPath";
 import BtnAction from "../components/BtnAction";
 type propsType = {};
@@ -11,7 +12,14 @@ export default function Homepage(props: propsType): React.JSX.Element {
   const goLink = useCallback((path: string) => {
     navigate(path);
   }, []);
+  const [step, setStep] = useState(0);
+  const [isOpenCreateProduct,setIsOpenCreateProduct] = useState(false);
 
+  const onOpenCreateProduct = useCallback(() => setIsOpenCreateProduct(true),[]);
+  const onCloseCreateProduct = useCallback(() => setIsOpenCreateProduct(false),[]);
+  const onChangeStep = (step: number) => {
+    setStep(step);
+  };
   return (
     <div className="homepage">
       <div className="homepage--overlay" />
@@ -32,12 +40,22 @@ export default function Homepage(props: propsType): React.JSX.Element {
         </Col>
         <Col>
           <BtnAction
-          onClick={() => goLink('/bill/create')}
+          onClick={onOpenCreateProduct}
             value="Thêm mới thuốc"
             icon={<MedicineBoxOutlined className="homepage--btnAction__icon" />}
           />
         </Col>
       </Row>
+      <Modal
+        
+          open={isOpenCreateProduct}
+          onCancel={onCloseCreateProduct}
+          footer={null}
+          width={1500}
+          destroyOnClose
+        >
+          <ShowStep onChangeStep={onChangeStep} onCloseModal={onCloseCreateProduct} step={step} />
+        </Modal>
     </div>
   );
 }
