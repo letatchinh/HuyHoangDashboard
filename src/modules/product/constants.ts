1ebcba9a09828e9ebc9a0a20b400202272522d2f
Lê Tất Chính <letatchinh123@gmail.com>
2024-02-20 21:30:46 +0700

    fix: add wholesale retail in Product Type

--- src/modules/product/constants.ts
+++ src/modules/product/constants.ts
@@ -1,10 +1,14 @@
 export const PRODUCT_TYPE = {
   exclusive: "exclusive",
   agency: "agency",
+  wholesale: 'wholesale',
+  retail: 'retail',
 } as const;
 export const PRODUCT_TYPE_VI = {
   exclusive: "Độc quyền",
   agency: "Đại lý",
+  wholesale: 'Sản phẩm bán buôn',
+  retail: 'Sản phẩm bán lẻ',
 } as const;
 
 export const SALE_LEVEL = {
