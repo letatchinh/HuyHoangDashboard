ec1b475e4a0c86629855229f96beda2d62e0962e
CuongVo <cuongvoquocc@gmail.com>
2024-04-03 08:47:15 +0700

    fix: DEFAULT_UPLOAD_ACTION

--- src/constants/defaultValue.ts
+++ src/constants/defaultValue.ts
@@ -3,7 +3,7 @@ import { devConfig } from "~/config";
 import { getOptions } from "~/utils/helpers";
 
 export const BASE_URL = 'config.REACT_APP_BASE_URL';
-export const DEFAULT_UPLOAD_ACTION = `${BASE_URL}api/v1/file`;
+export const DEFAULT_UPLOAD_ACTION = `${BASE_URL}/api/v1/file`;
 export const MAX_UPLOAD_FILE_SIZE_IN_MB = 2;
 
 
