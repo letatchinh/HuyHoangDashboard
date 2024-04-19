1a60d15fd66ad7b584405b80cad9df2859618b46
Nguyen <nguyenvom98@gmail.com>
2024-04-20 00:34:08 +0700

    fix: basic UI SalesGroup

--- src/modules/salesGroup/components/BuyGroup/index.tsx
+++ src/modules/salesGroup/components/BuyGroup/index.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
-type propsType = {
+import { ConfigProvider, Table } from "antd";
+import { ColumnProps } from "antd/es/table";
+import React from "react";
+import TableAnt from "~/components/Antd/TableAnt";
+type propsType = {};
 
-}
-export default function BuyGroup(props:propsType) : React.JSX.Element {
-    return (
-        <div>BuyGroup</div>
-    )
+
+const columns :ColumnProps<any>[]= [
+    {
+        title:'Tên',
+        key:'name',
+        dataIndex:'fullName'
+    }
+]
+
+export default function BuyGroup(props: propsType): React.JSX.Element {
+    
+  return (
+    <div className="buy_group" style={{ height: "200vh" }}>
+      <ConfigProvider
+        theme={{
+          components: {
+            Table: {
+              /* here is your component tokens */
+            },
+          },
+        }}
+      >
+        <TableAnt stickyTop sticky={{offsetHeader:0}} columns={columns}/>
+      </ConfigProvider>
+    </div>
+  );
 }
