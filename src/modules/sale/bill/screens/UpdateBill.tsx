fc14d00131ce2aba3d50eb066064d3e48815bb69
CuongVo <cuongvoquocc@gmail.com>
2024-08-14 16:16:08 +0700

    fix: #1974 reload bill Id

--- src/modules/sale/bill/screens/UpdateBill.tsx
+++ src/modules/sale/bill/screens/UpdateBill.tsx
@@ -338,6 +338,9 @@ export default function UpdateBill(props: propsType): React.JSX.Element {
           billId={id}
           defaultActiveTabKey="1"
           isNotSentTime
+          isRefetchBill={{
+            id: bill?._id
+            }}
         />
       </Modal>
     </div>
