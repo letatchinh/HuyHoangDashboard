70a05707f66a2f7ccdd602f8d52e8dffa499018a
Lê Tất Chính <letatchinh123@gmail.com>
2024-10-04 19:01:59 +0700

    fix: change Setup Pdf preview

--- src/core/pdfPreviewSetup.js
+++ src/core/pdfPreviewSetup.js
@@ -1,5 +1,2 @@
 import { pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
-    import.meta.url,
-  ).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
